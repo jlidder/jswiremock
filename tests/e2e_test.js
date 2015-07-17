0409ad6f6a7d1c81045696726fb0dec88da0fe16
jlidder@groupon.com <jlidder@groupon.com>
2015-07-17 15:39:21 -0700

    added travis ci info + testing additions

--- tests/e2e_test.js
+++ tests/e2e_test.js
@@ -1,13 +1,29 @@
 /**
  * Created by jlidder on 7/17/15.
  */
-var jswiremocklib, jswiremock, stubFor, get, urlEqualTo, a_response;
-jswiremocklib = require('../jswiremock'), jswiremock = jswiremocklib.jswiremock, stubFor = jswiremocklib.stubFor, get = jswiremocklib.get, urlEqualTo = jswiremocklib.urlEqualTo, a_response = jswiremocklib.a_response;
+var http = require('http');
+var assert = require('assert');
+var chai = require('chai');
+chai.should();
 
-var jswiremock = new jswiremock(5001);
+var jswiremocklib, stubFor, get, urlEqualTo, a_response;
+jswiremocklib = require('../jswiremock'), stubFor = jswiremocklib.stubFor, get = jswiremocklib.get, urlEqualTo = jswiremocklib.urlEqualTo, a_response = jswiremocklib.a_response;
 
-stubFor(jswiremock, get(urlEqualTo("/1"))
-    .willReturn(a_response()
-        .withStatus(200)
-        .withHeader({"Content-Type": "application/json"})
-        .withBody("[{\"status\":\"success\", \"custom_audience_id\":\"12345\", \"lookalike_audience_id\": \"678999\"}]")));
+describe('jswiremock library', function() {
+    describe('setup process', function() {
+        it('should return a mock_request and mock_response object with the right info', function() {
+            var mock_request = get(urlEqualTo("/1"))
+                                    .willReturn(a_response()
+                                        .withStatus(200)
+                                        .withHeader({"Content-Type": "application/json"})
+                                        .withBody("[{\"status\":\"success\", \"custom_audience_id\":\"12345\", \"lookalike_audience_id\": \"678999\"}]"));
+
+            assert.equal(mock_request.getUrl(), "/1", "URL do not match");
+            assert.equal(mock_request.getRequestType(), "GET");
+            var mock_response = mock_request.get_mock_response();
+            assert.equal(mock_response.getStatus(), 200);
+            assert.equal(mock_response.getBody(), "[{\"status\":\"success\", \"custom_audience_id\":\"12345\", \"lookalike_audience_id\": \"678999\"}]");
+            assert.deepEqual(mock_response.getHeader(), {"Content-Type": "application/json"})
+        });
+    });
+});
