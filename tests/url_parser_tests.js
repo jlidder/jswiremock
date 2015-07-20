64b894fa95a42d8a82540f00f48761a1a467df0e
jlidder@groupon.com <jlidder@groupon.com>
2015-07-20 12:26:15 -0700

    query params + dynamic query params

--- tests/url_parser_tests.js
+++ tests/url_parser_tests.js
@@ -29,6 +29,46 @@ describe('url_parser library', function() {
             assert.deepEqual(parent_node.getNext().getData(), ":test");
             assert.deepEqual(parent_node.getNext().getNext().getData(), "delete");
         });
+
+        it('should return a linked list of a url of /7/testacular?test=1&tester=2', function() {
+            var parent_node = url_parser.build_url_storage_linked_list("/7/testacular?test=1&tester=2");
+            assert.deepEqual(parent_node.getData(), "7");
+            assert.deepEqual(parent_node.getNext().getData(), "testacular");
+            assert.deepEqual(parent_node.getNext().getParams()["test"], "1");
+            assert.deepEqual(parent_node.getNext().getParams()["tester"], "2");
+        });
+
+        it('should return a linked list of a url of /testacular?test=1&tester=2', function() {
+            var parent_node = url_parser.build_url_storage_linked_list("/testacular?test=1&tester=2");
+            assert.deepEqual(parent_node.getData(), "testacular");
+            assert.deepEqual(parent_node.getParams()["test"], "1");
+            assert.deepEqual(parent_node.getParams()["tester"], "2");
+        });
+
+        it('should return a linked list of a url of /testacular/:wtv/ops?test=1&tester=2', function() {
+            var parent_node = url_parser.build_url_storage_linked_list("/testacular/:wtv/ops?test=1&tester=2");
+            assert.deepEqual(parent_node.getData(), "testacular");
+            assert.deepEqual(parent_node.getNext().getData(), ":wtv");
+            assert.deepEqual(parent_node.getNext().getNext().getData(), "ops");
+            assert.deepEqual(parent_node.getNext().getNext().getParams()["test"], "1");
+            assert.deepEqual(parent_node.getNext().getNext().getParams()["tester"], "2");
+        });
+        it('should return a linked list of a url of /testacular/:wtv/ops?test=1&tester=2', function() {
+            var parent_node = url_parser.build_url_storage_linked_list("/testacular/:wtv/:ops?test=1&tester=2");
+            assert.deepEqual(parent_node.getData(), "testacular");
+            assert.deepEqual(parent_node.getNext().getData(), ":wtv");
+            assert.deepEqual(parent_node.getNext().getNext().getData(), ":ops");
+            assert.deepEqual(parent_node.getNext().getNext().getParams()["test"], "1");
+            assert.deepEqual(parent_node.getNext().getNext().getParams()["tester"], "2");
+        });
+        it('should return a linked list of a url of /testacular/:wtv/ops?test=:var&tester=2', function() {
+            var parent_node = url_parser.build_url_storage_linked_list("/testacular/:wtv/:ops?test=:var&tester=2");
+            assert.deepEqual(parent_node.getData(), "testacular");
+            assert.deepEqual(parent_node.getNext().getData(), ":wtv");
+            assert.deepEqual(parent_node.getNext().getNext().getData(), ":ops");
+            assert.deepEqual(parent_node.getNext().getNext().getParams()["test"], ":var");
+            assert.deepEqual(parent_node.getNext().getNext().getParams()["tester"], "2");
+        });
     });
 
     //recursive_url_linked_list_search
@@ -38,31 +78,37 @@ describe('url_parser library', function() {
                 .willReturn(a_response()
                     .withStatus(200)
                     .withHeader({"Content-Type": "application/json"})
-                    .withBody("[{\"status\":\"success\", \"custom_audience_id\":\"12345\", \"lookalike_audience_id\": \"678999\"}]"));
+                    .withBody("[{\"status\":\"success\"}]"));
 
             var mock_request_2 = get(urlEqualTo("/5/:test/delete"))
                 .willReturn(a_response()
                     .withStatus(200)
                     .withHeader({"Content-Type": "application/json"})
-                    .withBody("[{\"status\":\"success\", \"custom_audience_id\":\"12345\", \"lookalike_audience_id\": \"678999\"}]"));
+                    .withBody("[{\"status\":\"success\"}]"));
 
             var mock_request_3 = get(urlEqualTo("/account/:varying_var/delete/"))
                 .willReturn(a_response()
                     .withStatus(200)
                     .withHeader({"Content-Type": "application/json"})
-                    .withBody("[{\"status\":\"success\", \"custom_audience_id\":\"12345\", \"lookalike_audience_id\": \"678999\"}]"));
+                    .withBody("[{\"status\":\"success\"}]"));
 
             var mock_request_4 = get(urlEqualTo("/:varying_var/delete/"))
                 .willReturn(a_response()
                     .withStatus(200)
                     .withHeader({"Content-Type": "application/json"})
-                    .withBody("[{\"status\":\"success\", \"custom_audience_id\":\"12345\", \"lookalike_audience_id\": \"678999\"}]"));
+                    .withBody("[{\"status\":\"success\"}]"));
 
             var mock_request_5 = get(urlEqualTo("/delete/:varying_var/"))
                 .willReturn(a_response()
                     .withStatus(200)
                     .withHeader({"Content-Type": "application/json"})
-                    .withBody("[{\"status\":\"success\", \"custom_audience_id\":\"12345\", \"lookalike_audience_id\": \"678999\"}]"));
+                    .withBody("[{\"status\":\"success\"}]"));
+
+            var mock_request_7 = get(urlEqualTo("/delete/:wtv/hello?meansit=:var"))
+                .willReturn(a_response()
+                    .withStatus(200)
+                    .withHeader({"Content-Type": "application/json"})
+                    .withBody("[{\"status\":\"success\"}]"));
 
             global.test_stubs = [];
             test_stubs.push(mock_request_1);
@@ -70,6 +116,7 @@ describe('url_parser library', function() {
             test_stubs.push(mock_request_3);
             test_stubs.push(mock_request_4);
             test_stubs.push(mock_request_5);
+            test_stubs.push(mock_request_7);
         });
         it('should find the right match for /5/:test/delete in stubs and receive url of /5/4536354345/delete', function() {
             assert.deepEqual(url_parser.has_matching_stub(url_parser.build_url_storage_linked_list("/5/4536354345/delete"), test_stubs) != null, true);
@@ -82,6 +129,9 @@ describe('url_parser library', function() {
         });
         it('should find the right match for /delete/:varying_var/ in stubs and receive url of /account/4536354345/delete', function() {
             assert.deepEqual(url_parser.has_matching_stub(url_parser.build_url_storage_linked_list("/4536354345/delete"), test_stubs) != null, true);
+        });
+        it('should find the right match for /delete/:wtv/hello?meansit=:var in stubs and receive url of /delete/1/hello?meansit=7', function() {
+            assert.deepEqual(url_parser.has_matching_stub(url_parser.build_url_storage_linked_list("/delete/1/hello?meansit=7"), test_stubs) != null, true);
         });
     });
 
--- url_parser.js
+++ url_parser.js
@@ -3,17 +3,45 @@
  */
 
 function url_node(data){
-    this.data = data;
-    this.next = null;
     this.append = function(next){
         this.next = next;
     };
     this.getData = function(){
-        return data;
+        return this.data;
     };
+    this.setData = function(data){
+        this.data = data;
+    }
     this.getNext = function(){
         return this.next;
     };
+    this.setParams = function(params){
+        var query_parameters = params.split("&");
+        if(query_parameters != null){
+            this.params = [];
+            for(var counter = 0; counter < query_parameters.length; counter++) {
+                var key_val_pair = query_parameters[counter].split("=");
+                this.params[key_val_pair[0]] = key_val_pair[1];
+            }
+        } else{
+            query_parameters = null;
+        }
+    };
+    this.getParams = function(){
+        return this.params;
+    };
+
+    //set var's for this object
+    var query_param_split = data.split("?");
+    this.params = null;
+    this.data = null;
+    this.next = null;
+    if(query_param_split != null && query_param_split.length > 1){
+        this.setData(query_param_split[0]);
+        this.setParams(query_param_split[1]);
+    } else{
+        this.setData(data);
+    }
 };
 
 exports.build_url_storage_linked_list = function(url){
@@ -66,9 +94,27 @@ function recursive_url_linked_list_search(receive_url_parts, stored_url_parts){
     // else RETURN FALSE
     if(stored_url_parts.getData().match(/(\:\w+)/) != null ||
         receive_url_parts.getData() === stored_url_parts.getData()){
+
+        if(receive_url_parts.getNext() == null && stored_url_parts.getNext() == null && receive_url_parts.getParams() != null){ //check if next query params are equal
+            return check_equal_query_params(receive_url_parts.getParams(), stored_url_parts.getParams());
+        }
+
         return recursive_url_linked_list_search(receive_url_parts.getNext(), stored_url_parts.getNext());
     } else{
         return false;
     }
 };
+
+function check_equal_query_params(received_query_params, stored_query_params){
+    for(key in received_query_params){
+        if(stored_query_params[key] != null){ //check if key exists in stored query params
+            if(stored_query_params[key] === received_query_params[key] || received_query_params[key].match(/(\:\w+)/) != null){
+                return true;
+            }
+        } else{
+            return false;
+        }
+    }
+    return true;
+}
 
